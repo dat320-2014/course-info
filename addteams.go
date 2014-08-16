f973e676dd9cb424ba25f5bbe3cd94eb5e9571d6
Hein Meling <hein.meling@uis.no>
2014-08-15 20:34:17 -0700

    addteams: added licence info.

--- addteams.go
+++ addteams.go
@@ -1,3 +1,16 @@
+// Copyright (C) 2014 Hein Meling.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package main
 
 import (
@@ -52,6 +65,10 @@ Troubleshooting:
 
 ** If you get a '404 Not Found []' message, please check that your personal
    access token has the necessary priviliges, e.g. to delete a repository.
+
+Author:
+
+** Copyright (C) 2014 Hein Meling.
 
 `
 
